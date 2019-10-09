dc8ec5014110bf0917ecd6d8b30f2ce097b3c6fc
Simon <s.drake03294@gmail.com>
2019-10-09 13:10:52 +0100

    screen and dial support

--- build/ux/canvas.js
+++ build/ux/canvas.js
@@ -36,24 +36,37 @@ export class Canvas {
         Canvas.width = Canvas.height * this.widthToHeightRatio;
     }
     static drawImages() {
-        var image = new Image();
-        image.src = '../../images/background_safe_minigame.png';
-        image.onload = () => {
-            Canvas.context.drawImage(image, 0, 0, Canvas.width, Canvas.height);
+        const shrinkFactor = Canvas.width / Canvas.maxWidth;
+        const background = new Image();
+        background.src = '../../images/background_safe_minigame.png';
+        background.onload = () => {
+            Canvas.context.drawImage(background, 0, 0, Canvas.width, Canvas.height);
         };
         const safe = new Image();
         safe.src = '../../images/safe_minigame.png';
-        console.log(Canvas.ratios["safe1"][0] * Canvas.width);
         safe.onload = () => {
-            Canvas.context.drawImage(safe, Canvas.ratios["safe1"][0] * Canvas.width, Canvas.ratios["safe1"][1] * Canvas.height);
-            // Canvas.context.drawImage(safe, unit*1.3,  unit*7);
-            // Canvas.context.drawImage(safe, unit*1.3,  unit*10);
-            // Canvas.context.drawImage(safe, unit*4.9,  unit*4);
-            // Canvas.context.drawImage(safe, unit*4.9,  unit*7);
-            // Canvas.context.drawImage(safe, unit*4.9,  unit*10);
-            // Canvas.context.drawImage(safe, unit*8.5,  unit*4);
-            // Canvas.context.drawImage(safe, unit*8.5,  unit*7);
-            // Canvas.context.drawImage(safe, unit*8.5,  unit*10);
+            const widthFactor = safe.width * shrinkFactor;
+            const heightFactor = safe.height * shrinkFactor;
+            // Make a loop?
+            Canvas.context.drawImage(safe, Canvas.ratios["safe1"][0] * Canvas.width, Canvas.ratios["safe1"][1] * Canvas.height, widthFactor, heightFactor);
+            Canvas.context.drawImage(safe, Canvas.ratios["safe2"][0] * Canvas.width, Canvas.ratios["safe2"][1] * Canvas.height, widthFactor, heightFactor);
+            Canvas.context.drawImage(safe, Canvas.ratios["safe3"][0] * Canvas.width, Canvas.ratios["safe3"][1] * Canvas.height, widthFactor, heightFactor);
+            Canvas.context.drawImage(safe, Canvas.ratios["safe4"][0] * Canvas.width, Canvas.ratios["safe4"][1] * Canvas.height, widthFactor, heightFactor);
+            Canvas.context.drawImage(safe, Canvas.ratios["safe5"][0] * Canvas.width, Canvas.ratios["safe5"][1] * Canvas.height, widthFactor, heightFactor);
+            Canvas.context.drawImage(safe, Canvas.ratios["safe6"][0] * Canvas.width, Canvas.ratios["safe6"][1] * Canvas.height, widthFactor, heightFactor);
+            Canvas.context.drawImage(safe, Canvas.ratios["safe7"][0] * Canvas.width, Canvas.ratios["safe7"][1] * Canvas.height, widthFactor, heightFactor);
+            Canvas.context.drawImage(safe, Canvas.ratios["safe8"][0] * Canvas.width, Canvas.ratios["safe8"][1] * Canvas.height, widthFactor, heightFactor);
+            Canvas.context.drawImage(safe, Canvas.ratios["safe9"][0] * Canvas.width, Canvas.ratios["safe9"][1] * Canvas.height, widthFactor, heightFactor);
+        };
+        const screen = new Image();
+        screen.src = '../../images/screen_safe_minigame.png';
+        screen.onload = () => {
+            Canvas.context.drawImage(screen, Canvas.ratios["screen"][0] * Canvas.width, Canvas.ratios["screen"][1] * Canvas.height, screen.width * shrinkFactor, screen.height * shrinkFactor);
+        };
+        const supportDial = new Image();
+        supportDial.src = '../../images/support_safe_dial_minigame.png';
+        supportDial.onload = () => {
+            Canvas.context.drawImage(supportDial, Canvas.ratios["supportDial"][0] * Canvas.width, Canvas.ratios["supportDial"][1] * Canvas.height, supportDial.width * shrinkFactor, supportDial.height * shrinkFactor);
         };
         // const image2 = new Image()
         // image2.src = '../../images/safe_open_minigame.png'
@@ -69,6 +82,17 @@ Canvas.maxHeight = 623;
 Canvas.widthToHeightRatio = Canvas.maxWidth / Canvas.maxHeight;
 Canvas.heightToWidthRatio = Canvas.maxHeight / Canvas.maxWidth;
 // How far left is it, how far down, 
+// Do divisions once?
 Canvas.ratios = {
-    "safe1": [(50 / Canvas.maxWidth), (180 / Canvas.maxHeight)]
+    "safe1": [(50 / Canvas.maxWidth), (173 / Canvas.maxHeight)],
+    "safe2": [(220 / Canvas.maxWidth), (173 / Canvas.maxHeight)],
+    "safe3": [(390 / Canvas.maxWidth), (173 / Canvas.maxHeight)],
+    "safe4": [(50 / Canvas.maxWidth), (320 / Canvas.maxHeight)],
+    "safe5": [(220 / Canvas.maxWidth), (320 / Canvas.maxHeight)],
+    "safe6": [(390 / Canvas.maxWidth), (320 / Canvas.maxHeight)],
+    "safe7": [(50 / Canvas.maxWidth), (467 / Canvas.maxHeight)],
+    "safe8": [(220 / Canvas.maxWidth), (467 / Canvas.maxHeight)],
+    "safe9": [(390 / Canvas.maxWidth), (467 / Canvas.maxHeight)],
+    "screen": [(578 / Canvas.maxWidth), (183 / Canvas.maxHeight)],
+    "supportDial": [(582 / Canvas.maxWidth), (280 / Canvas.maxHeight)]
 };
--- src/ux/canvas.ts
+++ src/ux/canvas.ts
@@ -8,8 +8,20 @@ export abstract class Canvas {
     public static heightToWidthRatio: number = Canvas.maxHeight / Canvas.maxWidth
     public static context: CanvasRenderingContext2D;
     // How far left is it, how far down, 
+    // Do divisions once?
     public static ratios: object = {
-        "safe1" : [(50/Canvas.maxWidth), (180/Canvas.maxHeight)]
+        "safe1" : [(50/Canvas.maxWidth), (173/Canvas.maxHeight)],
+        "safe2" : [(220/Canvas.maxWidth), (173/Canvas.maxHeight)],
+        "safe3" : [(390/Canvas.maxWidth), (173/Canvas.maxHeight)],
+        "safe4" : [(50/Canvas.maxWidth), (320/Canvas.maxHeight)],
+        "safe5" : [(220/Canvas.maxWidth), (320/Canvas.maxHeight)],
+        "safe6" : [(390/Canvas.maxWidth), (320/Canvas.maxHeight)],
+        "safe7" : [(50/Canvas.maxWidth), (467/Canvas.maxHeight)],
+        "safe8" : [(220/Canvas.maxWidth), (467/Canvas.maxHeight)],
+        "safe9" : [(390/Canvas.maxWidth), (467/Canvas.maxHeight)],
+        "screen" : [(578/Canvas.maxWidth), (183/Canvas.maxHeight)],
+        "supportDial" : [(582/Canvas.maxWidth), (280/Canvas.maxHeight)]
+
     }
 
     public static init(el: HTMLCanvasElement) {
@@ -53,25 +65,41 @@ export abstract class Canvas {
     }
 
     public static drawImages() {
+        const shrinkFactor = Canvas.width/Canvas.maxWidth
 
-        var image = new Image()
-        image.src = '../../images/background_safe_minigame.png'
-        image.onload = () => {
-            Canvas.context.drawImage(image, 0, 0, Canvas.width, Canvas.height);
+        const background = new Image()
+        background.src = '../../images/background_safe_minigame.png'
+        background.onload = () => {
+            Canvas.context.drawImage(background, 0, 0, Canvas.width, Canvas.height);
         }
+
         const safe = new Image()
         safe.src = '../../images/safe_minigame.png'
-        console.log(Canvas.ratios["safe1"][0]*Canvas.width)
         safe.onload = () => {
-            Canvas.context.drawImage(safe, Canvas.ratios["safe1"][0]*Canvas.width,  Canvas.ratios["safe1"][1]*Canvas.height);
-            // Canvas.context.drawImage(safe, unit*1.3,  unit*7);
-            // Canvas.context.drawImage(safe, unit*1.3,  unit*10);
-            // Canvas.context.drawImage(safe, unit*4.9,  unit*4);
-            // Canvas.context.drawImage(safe, unit*4.9,  unit*7);
-            // Canvas.context.drawImage(safe, unit*4.9,  unit*10);
-            // Canvas.context.drawImage(safe, unit*8.5,  unit*4);
-            // Canvas.context.drawImage(safe, unit*8.5,  unit*7);
-            // Canvas.context.drawImage(safe, unit*8.5,  unit*10);
+            const widthFactor = safe.width*shrinkFactor
+            const heightFactor = safe.height*shrinkFactor
+            // Make a loop?
+            Canvas.context.drawImage(safe, Canvas.ratios["safe1"][0]*Canvas.width,  Canvas.ratios["safe1"][1]*Canvas.height, widthFactor, heightFactor);
+            Canvas.context.drawImage(safe, Canvas.ratios["safe2"][0]*Canvas.width,  Canvas.ratios["safe2"][1]*Canvas.height, widthFactor, heightFactor);
+            Canvas.context.drawImage(safe, Canvas.ratios["safe3"][0]*Canvas.width,  Canvas.ratios["safe3"][1]*Canvas.height, widthFactor, heightFactor);
+            Canvas.context.drawImage(safe, Canvas.ratios["safe4"][0]*Canvas.width,  Canvas.ratios["safe4"][1]*Canvas.height, widthFactor, heightFactor);
+            Canvas.context.drawImage(safe, Canvas.ratios["safe5"][0]*Canvas.width,  Canvas.ratios["safe5"][1]*Canvas.height, widthFactor, heightFactor);
+            Canvas.context.drawImage(safe, Canvas.ratios["safe6"][0]*Canvas.width,  Canvas.ratios["safe6"][1]*Canvas.height, widthFactor, heightFactor);
+            Canvas.context.drawImage(safe, Canvas.ratios["safe7"][0]*Canvas.width,  Canvas.ratios["safe7"][1]*Canvas.height, widthFactor, heightFactor);
+            Canvas.context.drawImage(safe, Canvas.ratios["safe8"][0]*Canvas.width,  Canvas.ratios["safe8"][1]*Canvas.height, widthFactor, heightFactor);
+            Canvas.context.drawImage(safe, Canvas.ratios["safe9"][0]*Canvas.width,  Canvas.ratios["safe9"][1]*Canvas.height, widthFactor, heightFactor);
+        }
+
+        const screen = new Image()
+        screen.src = '../../images/screen_safe_minigame.png'
+        screen.onload = () => {
+            Canvas.context.drawImage(screen, Canvas.ratios["screen"][0]*Canvas.width,  Canvas.ratios["screen"][1]*Canvas.height, screen.width*shrinkFactor, screen.height*shrinkFactor);
+        }
+
+        const supportDial = new Image()
+        supportDial.src = '../../images/support_safe_dial_minigame.png'
+        supportDial.onload = () => {
+            Canvas.context.drawImage(supportDial, Canvas.ratios["supportDial"][0]*Canvas.width,  Canvas.ratios["supportDial"][1]*Canvas.height, supportDial.width*shrinkFactor, supportDial.height*shrinkFactor);
         }
 
         // const image2 = new Image()
